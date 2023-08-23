2b439227602757b783723ab4466dea9e071c8891
Arthi Kumar <arthi.k@solu-m.com>
2023-08-23 19:09:57 +0530

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h2>Employee Master</h2>
+  <div style="margin-top: 50px;" class="container">
+    
+    <div style="width:40%">
+      <v-card>
+      <v-card-title>
+        <p style="float: left;">Employee Info</p>
+        <div style="float: right;cursor: pointer;" @click="editDetails()"> <v-icon  style="font-size: 20px;" icon="mdi-pencil" />
+Edit</div>
+      </v-card-title>
+      <v-card-text >
+        <v-text-field    :disabled="isDisabled" v-model="displayName" placeholder="Display Name" outlined
+        dense
+        hide-details
+        clearable></v-text-field>
+        <v-text-field    :disabled="isDisabled" v-model="loginId" placeholder="Login ID" outlined
+        dense
+        hide-details
+        clearable></v-text-field>
+        <v-text-field   :disabled="isDisabled" v-model="password" placeholder="Password" outlined
+        dense
+        hide-details
+        clearable></v-text-field>
+      </v-card-text>
+      <v-card-action>
+        <v-btn  class="text-none ms-4 text-white"
+        color="blue-darken-4"
+        rounded="0"
+        variant="flat" @click="saveDetails()" :disabled="btnDisabled" style="margin-bottom: 10px;">Save</v-btn>
+      </v-card-action>
+    </v-card>
+    </div>
+    
+    <div style="width:40%">
+      <v-card>
+      <v-card-title >
+        <p style="float: left;">
+          View Employee ({{ storeEmpDetails.length }})
+        </p>
+        <div style="float: right;width:30%;">
+          <v-text-field
+        density="compact"
+        variant="solo"
+        label="Search"
+        append-inner-icon="mdi-magnify"
+        single-line
+        hide-details
+        v-model="searchValue"
+       
+       
+      ></v-text-field>
+        </div>
+       
+      </v-card-title>
+      <v-card-text>
+       <v-row  v-for="items in filteredList" :key="items.id">
+        <v-col ><v-avatar>
+      <v-img
+        src="https://i1.wp.com/sixseasonsreview.com/wp-content/uploads/2016/10/default.png?resize=300%2C300&ssl=1"
+        alt="Default image"
+      ></v-img>
+    </v-avatar>
+</v-col>
+        <v-col>{{ items.name }}</v-col>
+        <v-col>
+          <v-btn 
+            class="text-none ms-4 text-white" 
+            color="blue-darken-4" 
+            rounded="0" 
+            variant="flat"  
+            style="float: right;width:30%;margin-bottom: 10px;" :disabled="btnViewDisabled" :key="items.id" @click="viewDetails(items)">View</v-btn>
+        </v-col>
+       </v-row>
+     
+      </v-card-text>
+     
+    </v-card>
+    </div>
+    
+  </div>
   </div>
 </template>
 
@@ -35,24 +88,91 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      displayName:'',
+      loginId:'',
+      password:'',
+      storeEmpDetails: [],
+      isDisabled:false,
+      editFlag:false,
+      editEmpList:'',
+      btnDisabled:false,
+      btnViewDisabled:false,
+      searchValue:'',
+      getDetails:'',
+
+
+    }
+  },
+  computed:{
+    filteredList() {
+    return this.storeEmpDetails.filter(item => item.name.includes(this.searchValue) || item.loginId.includes(this.searchValue));
+  }
+  },
+
+
+  methods:{
+    saveDetails(){
+      if(this.displayName!== null && this.loginId!==null && this.password!==null && this.displayName!== '' && this.loginId!=='' && this.password!==''){
+      if(!this.editFlag){
+      this.storeEmpDetails.push({
+        id: this.storeEmpDetails.length+1,
+        name:this.displayName,
+        loginId:this.loginId,
+        password:this.password
+      })
+    }else{
+      this.btnViewDisabled=false
+
+      this.storeEmpDetails.filter((items)=>{
+        if(this.editEmpList.id===items.id){
+          items.name=this.displayName
+          items.loginId=this.loginId
+          items.password=this.password
+        }
+      })
+    }
+      this.filteredList=this.storeEmpDetails
+   
+  }
+    this.editEmpList=''
+      this.displayName=null
+      this.loginId=null
+      this.password=null
+    },
+    viewDetails(itemsData){
+      this.editEmpList=itemsData
+      this.isDisabled=true
+      this.btnDisabled=true
+      this.storeEmpDetails.filter((items)=>{
+        if(items.id===itemsData.id){
+          this.displayName=items.name
+          this.loginId=items.loginId
+          this.password=items.password
+
+        }
+      }) 
+    },
+    editDetails(){
+      if(this.storeEmpDetails.length!==0 && this.displayName!==''){
+      this.isDisabled=false
+      this.editFlag=true
+      this.btnDisabled=false
+      this.btnViewDisabled=true
+      }
+    },
+   
+  
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.container{
+  display: flex;
+  justify-content: space-around;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {  createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const Vuetify=createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi', // This is already the default value - only for display purposes
+      },
+})
+import 'vuetify/dist/vuetify.min.css'
+createApp(App).use(Vuetify).mount('#app')
